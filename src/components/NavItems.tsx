236a9df36c3c4332f2fc362c713f1edc1aa2dd73
Igor <igormraesmm@gmail.com>
2024-01-19 14:21:46 -0300

    feat: Add hook to close nav if click outside

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,16 +1,21 @@
 "use client"
 
 import { PRODUCT_CATEGORIES } from "@/config"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import NavItem from "./NavItem"
+import { useOnClickOutside } from "@/hooks/use-on-click-outside"
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null)
 
   const isAnyOpen = activeIndex !== null
 
+  const navRef = useRef<HTMLDivElement | null>(null)
+
+  useOnClickOutside(navRef, () => setActiveIndex(null))
+
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           if (activeIndex === i) {
