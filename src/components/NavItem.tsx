4713747f3d47b4597d3f5004e799e22a74233f99
Igor <igormraesmm@gmail.com>
2024-01-19 14:52:47 -0300

    feat: Add custom hooks

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -4,6 +4,8 @@ import { PRODUCT_CATEGORIES } from "@/config"
 import { Button } from "./ui/button"
 import { ChevronDown } from "lucide-react"
 import { cn } from "@/lib/utils"
+import Image from "next/image"
+import Link from "next/link"
 
 type Category = (typeof PRODUCT_CATEGORIES)[number]
 
@@ -31,6 +33,56 @@ const NavItem = ({ category, isAnyOpen, handleOpen, isOpen }: NavItemProps) => {
           />
         </Button>
       </div>
+
+      {isOpen ? (
+        <div
+          className={cn(
+            "absolute inset-x-0 top-full text-sm text-muted-foreground",
+            {
+              "animate-in fade-in-10 slide-in-from-top-5": !isAnyOpen,
+            }
+          )}
+        >
+          <div
+            className="absolute inset-0 top-1/2 bg-white shadow"
+            aria-hidden="true"
+          />
+
+          {/*Modal*/}
+          <div className="relative bg-white">
+            <div className="mx-auto max-w-7xl px-8">
+              <div className="grid grid-cols-4 gap-x-8 gap-y-10 py-16">
+                <div className="col-span-4 col-start-1 grid grid-cols-3 gap-x-8">
+                  {category.featured.map((item) => (
+                    <div
+                      key={item.name}
+                      className="group relative text-base sm:text-sm"
+                    >
+                      <div className="relative aspect-video overflow-hidden rounded-lg bg-gray-100 group-hover:opacity-75">
+                        <Image
+                          src={item.imageSrc}
+                          alt="product category image"
+                          fill
+                          className="object-cover object-center"
+                        />
+                      </div>
+                      <Link
+                        href={item.href}
+                        className="mt-6 block font-medium text-gray-900"
+                      >
+                        {item.name}
+                      </Link>
+                      <p className="mt-1" aria-hidden="true">
+                        Shop now
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
     </div>
   )
 }
--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,12 +1,26 @@
 "use client"
 
 import { PRODUCT_CATEGORIES } from "@/config"
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import NavItem from "./NavItem"
 import { useOnClickOutside } from "@/hooks/use-on-click-outside"
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null)
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null)
+      }
+    }
+
+    document.addEventListener("keydown", handler)
+
+    return () => {
+      document.removeEventListener("keydown", handler)
+    }
+  }, [])
 
   const isAnyOpen = activeIndex !== null
 
