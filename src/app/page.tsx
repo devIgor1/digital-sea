c2f500a8ddd28705ed3ba9b88991894cae91795c
Igor <igormraesmm@gmail.com>
2024-01-19 08:52:17 -0300

    feat: Finish landing page by now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,79 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import { Button, buttonVariants } from "@/components/ui/button"
+import { ArrowDownToLine, CheckCircle, Leaf } from "lucide-react"
 import Link from "next/link"
+
+const perks = [
+  {
+    name: "Instant Delivery",
+    Icon: ArrowDownToLine,
+    description:
+      "No waiting, no delays. Have your assets sent straight to your inbox for immediate download.",
+  },
+  {
+    name: "Guaranteed Quality",
+    Icon: CheckCircle,
+    description:
+      "Step into a world of assurance. Our products are crafted with precision, offering a guarantee of unparalleled quality. Not happy? We offer a 30-day refund guarantee.",
+  },
+  {
+    name: "For the Planet",
+    Icon: Leaf,
+    description:
+      "We are dedicated to environmental stewardship, allocating 1% of our sales towards the preservation and restoration of our planet's natural beauty.",
+  },
+]
 
 export default function Home() {
   return (
-    <MaxWidthWrapper>
-      <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
-        <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-          Your marketplace for high-quality{" "}
-          <span className="text-blue-600">digital assets</span>.
-        </h1>
-        <p className="mt-6 text-lg max-w-prose text-muted-foreground">
-          Welcome to Digital Sea. Every asset on our platform is verified by our
-          team to ensure our highest quality standards.
-        </p>
-        <div className="flex flex-col sm:flex-row gap-4 mt-6">
-          <Link href="/" className="">
-            Browse Trending
-          </Link>
+    <>
+      <MaxWidthWrapper>
+        <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+          <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+            Your marketplace for high-quality{" "}
+            <span className="text-blue-600">digital assets</span>.
+          </h1>
+          <p className="mt-6 text-lg max-w-prose text-muted-foreground">
+            Welcome to Digital Sea. Every asset on our platform is verified by
+            our team to ensure our highest quality standards.
+          </p>
+          <div className="flex flex-col sm:flex-row gap-4 mt-6">
+            <Link href="/" className={buttonVariants()}>
+              Browse Trending
+            </Link>
+            <Button variant="ghost">Our quality promise &rarr;</Button>
+          </div>
         </div>
-      </div>
-    </MaxWidthWrapper>
+
+        {/* TODO: List products */}
+      </MaxWidthWrapper>
+
+      <section className="border-t border-gray-200 bg-gray-50">
+        <MaxWidthWrapper className="py-20">
+          <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-0">
+            {perks.map((perk) => (
+              <div
+                key={perk.name}
+                className="text-center md:flex md:flex-start md:text-left lg:block lg:text-center"
+              >
+                <div className="md:flex-shrink-0 flex justify-center">
+                  <div className="h-16 w-16 flex items-center justify-center rounded-full bg-blue-100 text-blue-900">
+                    {<perk.Icon className="w-1/3 h-1/3" />}
+                  </div>
+                </div>
+                <div className="mt-6 md:ml-4 md:mt-0 lg:ml-0 lg:mt-6">
+                  <h3 className="text-base font-medium text-gray-900">
+                    {perk.name}
+                  </h3>
+                  <p className="mt-3 text-sm text-muted-foreground">
+                    {perk.description}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </MaxWidthWrapper>
+      </section>
+    </>
   )
 }
