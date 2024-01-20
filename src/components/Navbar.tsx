f1e5d3583f0111ab8bb2e3d7f730fe0270d62c49
Igor <igormraesmm@gmail.com>
2024-01-20 12:10:07 -0300

    feat: Add Cart component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,6 +3,7 @@ import MaxWidthWrapper from "./MaxWidthWrapper"
 import { Icons } from "./Icons"
 import NavItems from "./NavItems"
 import { buttonVariants } from "./ui/button"
+import Cart from "./Cart"
 
 const Navbar = () => {
   const user = null
