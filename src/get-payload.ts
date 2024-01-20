21b7f10f62b20a334bf342175321f0e188fd65cf
Igor <igormraesmm@gmail.com>
2024-01-20 15:04:22 -0300

    feat: Add dotenv

---
+++ src/get-payload.ts
@@ -0,0 +1,3 @@
+import dotenv from "dotenv"
+
+export const getPayloadClient = async () => {}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
 import express from "express"
+
+const app = express()
+const PORT = Number(process.env.PORT) || 3000
+
+const start = async () => {
+  const payload = await getPayloadClient()
+}
+
+start()
