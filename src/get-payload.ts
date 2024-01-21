7f4b47198ddacceda8955f44ae85c9c952404822
Igor <igormraesmm@gmail.com>
2024-01-21 13:02:37 -0300

    feat: Add comments

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -3,12 +3,15 @@ import path from "path"
 import payload from "payload"
 import type { InitOptions } from "payload/config"
 
+// Load environment variables from a .env file
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
 })
 
+// Global caching object for the Payload client
 let cached = (global as any).payload
 
+// If the cache doesn't exist, create it with client and promise properties set to null
 if (!cached) {
   cached = (global as any).payload = {
     client: null,
@@ -16,10 +19,12 @@ if (!cached) {
   }
 }
 
+// Define an interface for function arguments
 interface Args {
   initOptions?: Partial<InitOptions>
 }
 
+// Export a function to get or initialize the Payload client
 export const getPayloadClient = async ({ initOptions }: Args = {}) => {
   if (!process.env.PAYLOAD_SECRET) {
     throw new Error("PAYLOAD_SECRET is missing")
@@ -29,6 +34,7 @@ export const getPayloadClient = async ({ initOptions }: Args = {}) => {
     return cached.client
   }
 
+  // If the promise is not created yet, initialize Payload with options
   if (!cached.promise) {
     cached.promise = payload.init({
       secret: process.env.PAYLOAD_SECRET,
@@ -38,8 +44,10 @@ export const getPayloadClient = async ({ initOptions }: Args = {}) => {
   }
 
   try {
+    // Await the promise to initialize the client and store it in the cache
     cached.client = await cached.promise
   } catch (err: unknown) {
+    // If an error occurs during initialization, clear the promise and throw the error
     cached.promise = null
     throw err
   }
