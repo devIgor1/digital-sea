10b56dbda4156aefa927d9b86c7f9840e3f0b9ef
Igor <igormraesmm@gmail.com>
2024-01-21 16:36:00 -0300

    fix: Fix classnames props to the logo

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -3,6 +3,7 @@ import { LucideProps } from "lucide-react"
 export const Icons = {
   logo: (props: LucideProps) => (
     <svg
+      {...props}
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
       width="60"
