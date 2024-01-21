bf9cee424e58e5c22e3549c24279a2b65ea08e20
Igor <igormraesmm@gmail.com>
2024-01-21 09:55:51 -0300

    feat: Add payload

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
 import express from "express"
+import { getPayloadClient } from "./get-payload"
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
 
 const start = async () => {
-  const payload = await getPayloadClient()
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
+      },
+    },
+  })
 }
 
 start()
