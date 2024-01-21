ee0e9e596d99bf9bf039dd3946f8a4d8dfc9b6f3
Igor <igormraesmm@gmail.com>
2024-01-21 13:03:02 -0300

    feat: Admin dashboard configuration

---
+++ src/payload.config.ts
@@ -0,0 +1,31 @@
+import { mongooseAdapter } from "@payloadcms/db-mongodb"
+import { slateEditor } from "@payloadcms/richtext-slate"
+import { webpackBundler } from "@payloadcms/bundler-webpack"
+import { buildConfig } from "payload/config"
+import path from "path"
+
+export default buildConfig({
+  serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
+  collections: [], //Orders, products, product files, users, etc...
+  routes: {
+    admin: "/sell",
+  },
+  admin: {
+    bundler: webpackBundler(),
+    meta: {
+      titleSuffix: "-DigitalSea",
+      favicon: "/favicon.ico",
+      ogImage: "/thumbnail.jpg",
+    },
+  },
+  rateLimit: {
+    max: 2000,
+  },
+  editor: slateEditor({}),
+  db: mongooseAdapter({
+    url: process.env.MONGODB_URL!,
+  }),
+  typescript: {
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
+  },
+})
