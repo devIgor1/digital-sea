7f6092cb1ec8aaba6b8db88900b25b36a30134ff
Igor <igormraesmm@gmail.com>
2024-01-22 17:38:40 -0300

    feat: Add users collection configuration

---
+++ src/collections/users.ts
@@ -0,0 +1,20 @@
+import { CollectionConfig } from "payload/types"
+
+export const Users: CollectionConfig = {
+  slug: "users",
+  auth: true,
+  access: {
+    read: () => true,
+    create: () => true,
+  },
+  fields: [
+    {
+      name: "role",
+      type: "select",
+      options: [
+        { label: "Admin", value: "admin" },
+        { label: "User", value: "user" },
+      ],
+    },
+  ],
+}
