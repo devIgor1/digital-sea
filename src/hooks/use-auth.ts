1fb666add5075c19ce4ab288cfff608787da3470
Igor <igormraesmm@gmail.com>
2024-01-24 12:40:39 -0300

    feat: Create custom hook to sign out user

---
+++ src/hooks/use-auth.ts
@@ -0,0 +1,31 @@
+import { useRouter } from "next/navigation"
+import { toast } from "sonner"
+
+export const useAuth = () => {
+  const router = useRouter()
+
+  const signOut = async () => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/logout`,
+        {
+          method: "POST",
+          credentials: "include",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+
+      if (!res.ok) throw new Error()
+
+      toast.success("Signed out successfully")
+      router.push("/sign-in")
+      router.refresh()
+    } catch (error) {
+      toast.error("Couldn't sign out, please try again.")
+    }
+  }
+
+  return { signOut }
+}
