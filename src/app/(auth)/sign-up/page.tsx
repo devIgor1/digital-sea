fd885a3c14173ccf1629988ed2445d23890084fb
Igor <igormraesmm@gmail.com>
2024-01-24 10:02:58 -0300

    feat: Finish the send email verification functionality

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -13,8 +13,10 @@ import { trpc } from "@/trpc/client"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { ArrowRight } from "lucide-react"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 import { useForm } from "react-hook-form"
 import { toast } from "sonner"
+import { ZodError } from "zod"
 
 const Page = () => {
   const {
@@ -25,11 +27,27 @@ const Page = () => {
     resolver: zodResolver(AuthCredentialsValidator),
   })
 
+  const router = useRouter()
+
   const { mutate, isLoading } = trpc.auth.createPayloadUser.useMutation({
     onError: (err) => {
       if (err.data?.code === "CONFLICT") {
         toast.error("This email is already in use. Sign in instead?")
+
+        return
       }
+
+      if (err instanceof ZodError) {
+        toast.error(err.issues[0].message)
+
+        return
+      }
+
+      toast.error("Something went wrong. Please try again.")
+    },
+    onSuccess: ({ sentToEmail }) => {
+      toast.success(`Verification email sent to ${sentToEmail}`)
+      router.push("/verify-email?to=" + sentToEmail)
     },
   })
 
