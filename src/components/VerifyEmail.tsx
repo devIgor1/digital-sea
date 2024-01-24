6649547e8f5f04fe9bf7cdd524ce66dfde49f5fe
Igor <igormraesmm@gmail.com>
2024-01-24 14:40:52 -0300

    style: Fix wrong word

--- src/components/VerifyEmail.tsx
+++ src/components/VerifyEmail.tsx
@@ -21,7 +21,7 @@ const VerifyEmail = ({ token }: VerifyEmailProps) => {
         <XCircle className="h-8 w-8 text-red-600" />
         <h3 className="font-semibold text-xl">There was a problem</h3>
         <p className="text-muted-foreground text-sm text-center">
-          This token is not valid or mighht be expired. Please try again.
+          This token is not valid or might be expired. Please try again.
         </p>
       </div>
     )
