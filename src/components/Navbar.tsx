98af855169c08827878d9bb19dd97a1941fe3eed
Igor <igormraesmm@gmail.com>
2024-01-24 12:41:23 -0300

    feat: Add dropdown menu to logged in users navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,6 +6,7 @@ import { buttonVariants } from "./ui/button"
 import Cart from "./Cart"
 import { getServerSideUser } from "@/lib/payload-utils"
 import { cookies } from "next/headers"
+import UserAccountNav from "./UserAccountNav"
 
 const Navbar = async () => {
   const nextCookies = cookies()
@@ -43,7 +44,7 @@ const Navbar = async () => {
                   )}
 
                   {user ? (
-                    <p></p>
+                    <UserAccountNav user={user} />
                   ) : (
                     <Link
                       href="/sign-up"
