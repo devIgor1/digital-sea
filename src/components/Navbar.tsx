5de03061ce625f9009feb68d572ccee96b94ae85
Igor <igormraesmm@gmail.com>
2024-01-24 11:28:23 -0300

    feat: Get the cookies from user and make a dinamic navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,12 @@ import { Icons } from "./Icons"
 import NavItems from "./NavItems"
 import { buttonVariants } from "./ui/button"
 import Cart from "./Cart"
+import { getServerSideUser } from "@/lib/payload-utils"
+import { cookies } from "next/headers"
 
-const Navbar = () => {
-  const user = null
+const Navbar = async () => {
+  const nextCookies = cookies()
+  const { user } = await getServerSideUser(nextCookies)
 
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
