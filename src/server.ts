8f4a0db2c052507db10da30e11f4cfbd3d84b49c
Igor <igormraesmm@gmail.com>
2024-01-24 10:59:32 -0300

    feat: Sign-in functionality

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { getPayloadClient } from "./get-payload"
 import { nextApp, nextHandler } from "./next-utils"
 import * as trpcExpress from "@trpc/server/adapters/express"
 import { appRouter } from "./trpc"
+import { inferAsyncReturnType } from "@trpc/server"
 
 const app = express()
 const PORT = Number(process.env.PORT) || 3000
@@ -14,6 +15,8 @@ const createContext = ({
   req,
   res,
 })
+
+export type ExpressContext = inferAsyncReturnType<typeof createContext>
 
 const start = async () => {
   const payload = await getPayloadClient({
