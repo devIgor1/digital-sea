cde9949e685e138b6e52f534da8725b28351c806
Igor <igormraesmm@gmail.com>
2024-01-25 15:43:38 -0300

    feat: Add Products and ProductFiles collections

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,8 +5,9 @@ import { webpackBundler } from "@payloadcms/bundler-webpack"
 import { buildConfig } from "payload/config"
 import path from "path"
 import { Users } from "./collections/Users"
-import { Products } from "./collections/Products/products"
 import { Media } from "./collections/Media"
+import { Products } from "./collections/Products/Products"
+import { ProductFiles } from "./collections/ProductFile"
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -14,7 +15,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users, Products, Media], //Orders, products, product files, users, etc...
+  collections: [Users, Products, Media, ProductFiles], //Orders, products, product files, users, etc...
   routes: {
     admin: "/sell",
   },
