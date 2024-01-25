c2b2ea73f47462ae749f15397d8c8410cf11a587
Igor <igormraesmm@gmail.com>
2024-01-25 09:49:10 -0300

    feat: Finish products collection

--- src/collections/Products/products.ts
+++ src/collections/Products/products.ts
@@ -1,3 +1,4 @@
+import { PRODUCT_CATEGORIES } from "../../config/index"
 import { CollectionConfig } from "payload/types"
 
 export const Products: CollectionConfig = {
@@ -9,10 +10,10 @@ export const Products: CollectionConfig = {
   fields: [
     {
       name: "user",
-      type: "relationship",
+      type: "relationship", //If the type was 'select' for example, then I wouldn't be able to put the 'relationTo' field
       relationTo: "users",
       required: true,
-      hasMany: true,
+      hasMany: false,
       admin: {
         condition: () => false,
       },
@@ -22,6 +23,104 @@ export const Products: CollectionConfig = {
       label: "Name",
       type: "text",
       required: true,
+    },
+    {
+      name: "description",
+      type: "textarea",
+      label: "Product Details",
+    },
+    {
+      name: "price",
+      label: "Price in USD",
+      min: 0,
+      max: 1000,
+      type: "number",
+      required: true,
+    },
+    {
+      name: "category",
+      label: "Category",
+      type: "select",
+      options: PRODUCT_CATEGORIES.map(({ label, value }) => ({ label, value })),
+      required: true,
+    },
+    {
+      name: "product_files",
+      label: "Product File(s)",
+      type: "relationship",
+      required: true,
+      relationTo: "product_files",
+      hasMany: false,
+    },
+    {
+      name: "approvedForSale",
+      label: "Product Status",
+      type: "select",
+      defaultValue: "pending",
+      access: {
+        create: ({ req }) => req.user.role === "admin",
+        read: ({ req }) => req.user.role === "admin",
+        update: ({ req }) => req.user.role === "admin",
+      },
+      options: [
+        {
+          label: "Pending verification",
+          value: "pending",
+        },
+        {
+          label: "Approved",
+          value: "approved",
+        },
+        {
+          label: "Denied",
+          value: "denied",
+        },
+      ],
+    },
+
+    {
+      name: "priceId",
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: "text",
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: "stripeId",
+      access: {
+        create: () => false,
+        read: () => false,
+        update: () => false,
+      },
+      type: "text",
+      admin: {
+        hidden: true,
+      },
+    },
+    {
+      name: "images",
+      type: "array",
+      label: "Product Images",
+      minRows: 1,
+      maxRows: 4,
+      required: true,
+      labels: {
+        singular: "Image",
+        plural: "Images",
+      },
+      fields: [
+        {
+          name: "image",
+          type: "upload",
+          relationTo: "media  ",
+          required: true,
+        },
+      ],
     },
   ],
 }
