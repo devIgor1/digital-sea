84d4a3fdb28afaa8d5d1f085e520c90d439d4004
Igor <igormraesmm@gmail.com>
2024-01-26 17:34:00 -0300

    style: Remove unnecessary style from inactiveStyles

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -36,7 +36,7 @@ const ImageSlider = ({ urls }: ImageSliderProps) => {
   const activeStyles =
     "active:scale-[0.97] grid opacity-100 hover:scale-105 absolute top-1/2 -translate-y-1/2 aspect-square h-8 w-8 place-items-center rounded-full border-2 bg-white border-zinc-300"
 
-  const inactiveStyles = "hidden text-gray-400"
+  const inactiveStyles = "hidden"
   return (
     <div className="group relative bg-zinc-100 aspect-square overflow-hidden rounded-xl">
       <div className="absolute z-10 inset-0 opacity-0 group-hover:opacity-100 transition">
