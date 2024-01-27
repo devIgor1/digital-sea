60df04749f7b2056b9edbaaaacd0032a8767d324
Igor <igormraesmm@gmail.com>
2024-01-27 11:57:00 -0300

    feat: Adding item to cart

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -54,7 +54,6 @@ const Page = async ({ params }: PageProps) => {
     <MaxWidthWrapper className="bg-white">
       <div className="bg-white">
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
-          {/* Product Details */}
           <div className="lg:max-w-lg lg:self-end">
             <ol className="flex items-center space-x-2">
               {BREADCRUMBS.map((breadcrumb, i) => (
@@ -121,7 +120,7 @@ const Page = async ({ params }: PageProps) => {
           <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
             <div>
               <div className="mt-10">
-                <AddToCartButton />
+                <AddToCartButton product={product} />
               </div>
               <div className="mt-6 text-center">
                 <div className="group inline-flex text-sm font-medium">
