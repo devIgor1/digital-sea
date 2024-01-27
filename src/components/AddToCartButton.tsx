88fb8968663c0d5d396b9c29ea0d07b69b008912
Igor <igormraesmm@gmail.com>
2024-01-27 11:17:18 -0300

    feat: Finish AddToCartComponent

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,9 +1,32 @@
-import { Button } from "./ui/button"
+"use client"
 
-const AddToCartButton = () => {
+import { useEffect, useState } from "react"
+import { Button } from "./ui/button"
+import { useCart } from "@/hooks/use-cart"
+import { Product } from "@/payload-types"
+
+const AddToCartButton = ({ product }: { product: Product }) => {
+  const { addItem } = useCart()
+  const [isSuccess, setIsSuccess] = useState<boolean>(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsSuccess(false)
+    }, 2000)
+
+    return () => clearTimeout(timeout)
+  }, [isSuccess])
+
   return (
-    <Button size="lg" className="w-full">
-      Add to cart
+    <Button
+      size="lg"
+      className="w-full"
+      onClick={() => {
+        addItem(product)
+        setIsSuccess(true)
+      }}
+    >
+      {isSuccess ? "Added!" : "Add to cart"}
     </Button>
   )
 }
