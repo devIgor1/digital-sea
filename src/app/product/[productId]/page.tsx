c7cf219eafaf2ae37f82a1ecde50dcb631c5e2e2
Igor <igormraesmm@gmail.com>
2024-01-27 10:35:45 -0300

    feat: Add product reel

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,9 +1,10 @@
 import ImageSlider from "@/components/ImageSlider"
 import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+import ProductReel from "@/components/ProductReel"
 import { PRODUCT_CATEGORIES } from "@/config"
 import { getPayloadClient } from "@/get-payload"
 import { formatPrice } from "@/lib/utils"
-import { Check } from "lucide-react"
+import { Check, Shield } from "lucide-react"
 import Link from "next/link"
 import { notFound } from "next/navigation"
 
@@ -116,8 +117,33 @@ const Page = async ({ params }: PageProps) => {
               <ImageSlider urls={validUrls} />
             </div>
           </div>
+          <div className="mt-10 lg:col-start-1 lg:row-start-2 lg:max-w-lg lg:self-start">
+            <div>
+              <div className="mt-10">
+                <AddToCartButton />
+              </div>
+              <div className="mt-6 text-center">
+                <div className="group inline-flex text-sm font-medium">
+                  <Shield
+                    aria-hidden="true"
+                    className="mr-2 h-5 w-5 flex-shrink-0 text-gray-400"
+                  />
+                  <span className="text-muted-foreground hover:text-gray-700">
+                    30 Day Return Guarantee
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
+      <ProductReel
+        href="/products"
+        query={{ category: product.category, limit: 4 }}
+        title={`Similar ${label}`}
+        subtitle={`Browse similar high-quality ${label} just like '${product.name}'`}
+      />
     </MaxWidthWrapper>
   )
 }
