0332652a4d9adc21ffe636ac537f99a8f7772848
Igor <igormraesmm@gmail.com>
2024-01-28 19:00:32 -0300

    build: Build

--- dist/collections/Products/Products.js
+++ dist/collections/Products/Products.js
@@ -46,6 +46,15 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Products = void 0;
 var config_1 = require("../../config");
@@ -60,13 +69,78 @@ var addUser = function (_a) {
         });
     });
 };
+var syncUser = function (_a) {
+    var req = _a.req, doc = _a.doc;
+    return __awaiter(void 0, void 0, void 0, function () {
+        var fullUser, products, allIDs_1, createdProductIDs, dataToUpdate;
+        return __generator(this, function (_b) {
+            switch (_b.label) {
+                case 0: return [4 /*yield*/, req.payload.findByID({
+                        collection: "users",
+                        id: req.user.id,
+                    })];
+                case 1:
+                    fullUser = _b.sent();
+                    if (!(fullUser && typeof fullUser === "object")) return [3 /*break*/, 3];
+                    products = fullUser.products;
+                    allIDs_1 = __spreadArray([], ((products === null || products === void 0 ? void 0 : products.map(function (product) {
+                        return typeof product === "object" ? product.id : product;
+                    })) || []), true);
+                    createdProductIDs = allIDs_1.filter(function (id, index) { return allIDs_1.indexOf(id) === index; });
+                    dataToUpdate = __spreadArray(__spreadArray([], createdProductIDs, true), [doc.id], false);
+                    return [4 /*yield*/, req.payload.update({
+                            collection: "users",
+                            id: fullUser.id,
+                            data: {
+                                products: dataToUpdate,
+                            },
+                        })];
+                case 2:
+                    _b.sent();
+                    _b.label = 3;
+                case 3: return [2 /*return*/];
+            }
+        });
+    });
+};
+var isAdminOrHasAccess = function () {
+    return function (_a) {
+        var _user = _a.req.user;
+        var user = _user;
+        if (!user)
+            return false;
+        if (user.role === "admin")
+            return true;
+        var userProductIDs = (user.products || []).reduce(function (acc, product) {
+            if (!product)
+                return acc;
+            if (typeof product === "string") {
+                acc.push(product);
+            }
+            else {
+                acc.push(product.id);
+            }
+            return acc;
+        }, []);
+        return {
+            id: {
+                in: userProductIDs,
+            },
+        };
+    };
+};
 exports.Products = {
     slug: "products",
     admin: {
         useAsTitle: "name",
     },
-    access: {},
+    access: {
+        read: isAdminOrHasAccess(),
+        update: isAdminOrHasAccess(),
+        delete: isAdminOrHasAccess(),
+    },
     hooks: {
+        afterChange: [syncUser],
         beforeChange: [
             addUser,
             function (args) { return __awaiter(void 0, void 0, void 0, function () {
@@ -107,7 +181,7 @@ exports.Products = {
     fields: [
         {
             name: "user",
-            type: "relationship", //If the type was 'select' for example, then I wouldn't be able to put the 'relationTo' field
+            type: "relationship",
             relationTo: "users",
             required: true,
             hasMany: false,
@@ -213,7 +287,7 @@ exports.Products = {
         {
             name: "images",
             type: "array",
-            label: "Product Images",
+            label: "Product images",
             minRows: 1,
             maxRows: 4,
             required: true,
--- dist/payload.config.js
+++ dist/payload.config.js
@@ -8,12 +8,12 @@ var bundler_webpack_1 = require("@payloadcms/bundler-webpack");
 var db_mongodb_1 = require("@payloadcms/db-mongodb");
 var richtext_slate_1 = require("@payloadcms/richtext-slate");
 var path_1 = __importDefault(require("path"));
-var Users_1 = require("./collections/Users");
 var dotenv_1 = __importDefault(require("dotenv"));
 var Products_1 = require("./collections/Products/Products");
 var Media_1 = require("./collections/Media");
 var ProductFile_1 = require("./collections/ProductFile");
 var Orders_1 = require("./collections/Orders");
+var Users_1 = require("./collections/Users");
 dotenv_1.default.config({
     path: path_1.default.resolve(__dirname, "../.env"),
 });
