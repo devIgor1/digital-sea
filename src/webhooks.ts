be3c709b60cfb3855a1088bf32d2c59b5bdb64ca
Igor <igormraesmm@gmail.com>
2024-01-28 18:08:56 -0300

    refactor: Put my email

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -83,7 +83,7 @@ export const stripeWebhookHandler = async (
     // send receipt
     try {
       const data = await resend.emails.send({
-        from: "DigitalHippo <hello@joshtriedcoding.com>",
+        from: "Digital Sea <hello@codedbyigor.com>",
         to: [user.email],
         subject: "Thanks for your order! This is your receipt.",
         html: ReceiptEmailHtml({
