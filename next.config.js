0d7f8474a8018d4871311506b15eb9e183c48910
Igor <igormraesmm@gmail.com>
2024-01-29 15:17:30 -0300

    fix: Images load

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "digital-sea-production.up.railway.app"],
+    remotePatterns: [
+      {
+        hostname: "localhost",
+        pathname: "**",
+        protocol: "http",
+        port: "3000",
+      },
+      {
+        hostname: "digital-sea-production.up.railway.app",
+        protocol: "https",
+      },
+    ],
   },
 }
 
