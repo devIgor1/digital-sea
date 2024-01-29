ef98bb1f4a4a1f08d8951fcdfb50589674cd290e
Igor <igormraesmm@gmail.com>
2024-01-29 11:01:50 -0300

    fix: test

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -16,7 +16,6 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users, Products, Media, ProductFiles, Orders],
   routes: {
     admin: "/sell",
   },
