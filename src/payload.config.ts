bcf21cc073f629a84e583e1d6f5726bd6fdf6cbd
Igor <igormraesmm@gmail.com>
2024-01-29 08:57:26 -0300

    fix: Try to fix deploy error

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,12 +3,12 @@ import { webpackBundler } from "@payloadcms/bundler-webpack"
 import { mongooseAdapter } from "@payloadcms/db-mongodb"
 import { slateEditor } from "@payloadcms/richtext-slate"
 import path from "path"
-import { Users } from "./collections/Users"
+import { Users } from "@/collections/Users"
 import dotenv from "dotenv"
-import { Products } from "./collections/Products/Products"
-import { Media } from "./collections/Media"
-import { ProductFiles } from "./collections/ProductFile"
-import { Orders } from "./collections/Orders"
+import { Products } from "@/collections/Products/Products"
+import { Media } from "@/collections/Media"
+import { ProductFiles } from "@/collections/ProductFile"
+import { Orders } from "@/collections/Orders"
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
