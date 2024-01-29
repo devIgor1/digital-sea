c2081399552d17e9e796c829db25141a04bf7ff7
Igor <igormraesmm@gmail.com>
2024-01-29 13:51:16 -0300

    fix: Images loading on production

--- next.config.js
+++ next.config.js
@@ -1,16 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost",
-      },
-      {
-        protocol: "https",
-        hostname: "https://digital-sea-production.up.railway.app",
-      },
-    ],
+    domains: ["localhost", "https://digital-sea-production.up.railway.app"],
   },
 }
 
