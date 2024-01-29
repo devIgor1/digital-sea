93b9ad12dc27b087b94f4e3c6e73d7d273b2ad2c
Igor <igormraesmm@gmail.com>
2024-01-29 11:29:24 -0300

    feat: Add collections

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,6 +5,10 @@ import { slateEditor } from "@payloadcms/richtext-slate"
 import path from "path"
 import dotenv from "dotenv"
 import { Users } from "./collections/Users"
+import { Products } from "./collections/Products"
+import { Orders } from "./collections/Orders"
+import { Media } from "./collections/Media"
+import { ProductFiles } from "./collections/ProductFile"
 
 dotenv.config({
   path: path.resolve(__dirname, "../.env"),
@@ -12,7 +16,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users],
+  collections: [Users, Products, Orders, Media, ProductFiles],
   routes: {
     admin: "/sell",
   },
