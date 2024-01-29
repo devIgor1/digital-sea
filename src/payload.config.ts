004971ae38410b90b0b7f4ef61f3ca1090334b59
Igor <igormraesmm@gmail.com>
2024-01-29 08:42:08 -0300

    fix: Try to fix deploy error

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -24,7 +24,7 @@ export default buildConfig({
     user: "users",
     bundler: webpackBundler(),
     meta: {
-      titleSuffix: "- Digital Sea",
+      titleSuffix: "- DigitalSea",
       favicon: "/favicon.ico",
       ogImage: "/thumbnail.jpg",
     },
