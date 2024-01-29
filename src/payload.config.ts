16599f23788d6955efcabce75af613252e4b7be5
Igor <igormraesmm@gmail.com>
2024-01-29 08:36:12 -0300

    fix: Try to fix deploy error

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -3,7 +3,7 @@ import { webpackBundler } from "@payloadcms/bundler-webpack"
 import { mongooseAdapter } from "@payloadcms/db-mongodb"
 import { slateEditor } from "@payloadcms/richtext-slate"
 import path from "path"
-import { Users } from "../src/collections/Users"
+
 import dotenv from "dotenv"
 import { Products } from "./collections/Products/Products"
 import { Media } from "./collections/Media"
@@ -16,7 +16,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
-  collections: [Users, Products, Media, ProductFiles, Orders],
+  collections: [Products, Media, ProductFiles, Orders],
   routes: {
     admin: "/sell",
   },
