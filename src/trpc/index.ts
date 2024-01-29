3c2022e1bae86f475a5bbac1fd8a570690c4019f
Igor <igormraesmm@gmail.com>
2024-01-29 09:44:03 -0300

    fix: Fix some imports

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,29 +3,30 @@ import { authRouter } from "./auth-router"
 import { publicProcedure, router } from "./trpc"
 import { QueryValidator } from "../lib/validators/query-validator"
 import { getPayloadClient } from "../get-payload"
-import { paymentRouter } from "./payment.router"
+import { paymentRouter } from "./payment-router"
 
 export const appRouter = router({
   auth: authRouter,
   payment: paymentRouter,
+
   getInfiniteProducts: publicProcedure
     .input(
       z.object({
         limit: z.number().min(1).max(100),
-        cursor: z.number().nullish(), //last product
+        cursor: z.number().nullish(),
         query: QueryValidator,
       })
     )
     .query(async ({ input }) => {
       const { query, cursor } = input
-      const { sort, limit, ...queryOptions } = query
+      const { sort, limit, ...queryOpts } = query
 
       const payload = await getPayloadClient()
 
-      const parsedQueryOptions: Record<string, { equals: string }> = {}
+      const parsedQueryOpts: Record<string, { equals: string }> = {}
 
-      Object.entries(queryOptions).forEach(([key, value]) => {
-        parsedQueryOptions[key] = {
+      Object.entries(queryOpts).forEach(([key, value]) => {
+        parsedQueryOpts[key] = {
           equals: value,
         }
       })
@@ -33,7 +34,7 @@ export const appRouter = router({
       const page = cursor || 1
 
       const {
-        docs: products,
+        docs: items,
         hasNextPage,
         nextPage,
       } = await payload.find({
@@ -42,7 +43,7 @@ export const appRouter = router({
           approvedForSale: {
             equals: "approved",
           },
-          ...parsedQueryOptions,
+          ...parsedQueryOpts,
         },
         sort,
         depth: 1,
@@ -51,7 +52,7 @@ export const appRouter = router({
       })
 
       return {
-        products,
+        items,
         nextPage: hasNextPage ? nextPage : null,
       }
     }),
