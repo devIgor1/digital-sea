dd264012e43aaad024890ed55dbe90d338f1e090
Igor <igormraesmm@gmail.com>
2024-01-29 14:38:40 -0300

    feat: Change welcome image

--- src/components/emails/PrimaryActionEmail.tsx
+++ src/components/emails/PrimaryActionEmail.tsx
@@ -32,7 +32,7 @@ export const EmailTemplate = ({
       <Body style={main}>
         <Container style={container}>
           <Img
-            src={`${process.env.NEXT_PUBLIC_SERVER_URL}/primary-action-email.png`}
+            src={`${process.env.NEXT_PUBLIC_SERVER_URL}/welcome-penguin.jpg`}
             width="150"
             height="150"
             alt="DigitalSea"
